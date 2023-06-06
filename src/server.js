b295047ecc0f093bd6f83881823c41452fcc9e57
Fernando <97592954+calcifersh@users.noreply.github.com>
2023-06-07 00:02:01 +0200

    nodejs

--- src/server.js
+++ src/server.js
@@ -14,6 +14,46 @@ app.get('/',function(req,res,next){
   res.send('Funciona correctamente');
 })
 
+app.get('/contacto.html', (req, res) => {
+  res.sendFile('contacto.html', { root: __dirname + '/public' });
+});
+
 app.listen(app.get('port'),()=>{
   console.log('Server online 0.1');
 })
+
+
+
+/* CAPCHA DE GOOGLE */
+
+const axios = require('axios');
+const siteKey = '6Lc-KnUmAAAAADO9Z_BUo8ez4gKHmwvyPt6cBhCP';
+const secretKey = 'KnUmAAAAABkkE17vUjXeOYNPpvnvVjnhNWVk';
+
+app.post('/verificar-captcha', (req, res) => {
+  const captchaToken = req.body.token;
+
+  axios
+    .post('https://www.google.com/recaptcha/api/siteverify', null, {
+      params: {
+        secret: secretKey,
+        response: captchaToken,
+      },
+    })
+    .then(response => {
+      const { success } = response.data;
+
+      if (success) {
+        // El CAPTCHA se verificó correctamente, puedes continuar con el proceso
+        res.send('CAPTCHA verificado correctamente');
+      } else {
+        // El CAPTCHA no se verificó correctamente, toma las medidas adecuadas
+        res.send('CAPTCHA no válido');
+      }
+    })
+    .catch(error => {
+      // Ocurrió un error al verificar el CAPTCHA
+      console.error('Error al verificar el CAPTCHA:', error);
+      res.status(500).send('Error al verificar el CAPTCHA');
+    });
+});
